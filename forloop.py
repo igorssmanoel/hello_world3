d49c1f1237c6102fd7e186f5cc00ad4e8d710cfd
Igor Manoel <igorssmanoel@gmail.com>
2022-02-10 19:29:03 -0300

    forloop

---
+++ forloop.py
@@ -0,0 +1,13 @@
+# for while
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+for i in range(0, 10, 3):
+    print(f"{i} hello world")
+
+numero = int(input())
+while numero != 7:
+    print(f"O numero digitado foi {numero}")
+    numero = int(input())
